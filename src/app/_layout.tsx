650b3459ae93c5824e55fd87d50efce63ff3b8f2
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2025-10-13 01:16:07 -0600

    Initial config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,18 @@
-import { Stack } from "expo-router";
 import "../global.css";
+import ModernTabsNavigation from "@/src/shared/components/navigation/ModernTabsNavigation";
+import NativeTabsNavigation from "@/src/shared/components/navigation/NativeTabsNavigation";
+import {studentsRoutesNative} from "@/src/students/routes/studentsRoutesNative";
+import {studentsJavascriptRoutes} from "@/src/students/routes/studentsJavascriptRoutes";
+import JavaScriptTabsNavigation from "@/src/shared/components/navigation/JavaScriptTabsNavigation";
+import {studentsRoutes} from "@/src/students/routes/studentsRoutesModern";
+
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+        <>
+            {/*<JavaScriptTabsNavigation routes={studentsJavascriptRoutes} />*/}
+            <ModernTabsNavigation routes={studentsRoutes} />
+            {/*<NativeTabsNavigation routes={studentsRoutesNative} />*/}
+        </>
+    );
 }
